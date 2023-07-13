bef706bf4a9070e01679cdaa84171b665909c3e8
peruso <michaelyuta@live.jp>
2023-07-13 17:22:34 -0400

    Installed Sanctum and confirmed connection with DB

--- config/database.php
+++ config/database.php
@@ -65,12 +65,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', 'dpg-cims8qunqqlchhmhlqkg-a.oregon-postgres.render.com'),
+            'port' => env('DB_PORT', 5432),
+            'database' => env('DB_DATABASE', 'opinionet'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'l6VHeUGfNtze8XlLoF3JLK8uaOf71s7l'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -125,7 +124,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
