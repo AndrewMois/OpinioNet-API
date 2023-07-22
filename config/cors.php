1d42cf82664062aa534a47b64e16578b2c1ca222
Andrew <amoiseyenko@ukr.net>
2023-07-22 15:50:56 -0400

    Trying to overcome CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'https://opinionet-front-end.vercel.app')],
+    'allowed_origins' => [env('FRONTEND_URL', 'localhost:3000')],
 
     'allowed_origins_patterns' => [],
 
