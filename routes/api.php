c4cd7cb67ecccfc9417c742617b40a3cdd8d35d5
peruso <michaelyuta@live.jp>
2023-07-24 10:44:39 -0400

    Created some protected root. Still comment out.

--- routes/api.php
+++ routes/api.php
@@ -15,12 +15,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+//The restriction applies to the GET request for the /user endpoint. It allows authenticated users to retrieve their own information.
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 // Route::get('/users', [UserController::class, 'index']);
+
+//upproteted routes
 Route::apiResource('/users', 'App\Http\Controllers\UserController');
-Route::apiResource('/microposts', 'App\Http\Controllers\MicropostController');
 Route::put('/microposts/{id}/addLikes', [MicropostController::class, 'addLikes']);
+Route::apiResource('/microposts', 'App\Http\Controllers\MicropostController');
+
+//protected routes
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Route::apiResource('/users', 'App\Http\Controllers\UserController');
+    // Route::put('/microposts/{id}/addLikes', [MicropostController::class, 'addLikes']);
+    //It depends on http action and intended function in the controller. Need to discuss.
+    // Route::apiResource('/microposts', 'App\Http\Controllers\MicropostController');
+});
