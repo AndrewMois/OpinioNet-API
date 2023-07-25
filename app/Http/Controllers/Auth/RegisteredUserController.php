7b274e36f295f64bec23d87071a5ab002dfec5e1
Andrew <amoiseyenko@ukr.net>
2023-07-24 22:28:57 -0400

    cookies are now attached to the registration response

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,10 +43,18 @@ class RegisteredUserController extends Controller
         // return response()->noContent();
         //create user token
         $token = $user->createToken('auth_token')->plainTextToken;
+
         //return token as json
-        return response()->json([
+        $response = response()->json([
             'token' => $token,
             'user_id' => $user->id,
-            ]);
+        ]);
+
+        // Set the cookies with the desired expiration time (in minutes)
+        $response->withCookie('token', $token, 1440); // Expiration is 24 hours
+        $response->withCookie('user_id', $user->id, 1440); // Expiration is 24 hours
+
+        return $response;
+
     }
 }
