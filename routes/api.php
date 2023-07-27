ecb4a49cf9ea18981833609d5d244e1cd2b37930
Andrew <amoiseyenko@ukr.net>
2023-07-27 15:01:40 -0400

    fixing route protection

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 //unproteted routes
 //To show microposts for a specifc user
-// Route::get('/users/{id}/microposts', [UserController::class, 'userShowMicroposts']);
+ Route::get('/users/{id}/microposts', [UserController::class, 'userShowMicroposts']);
  Route::apiResource('/users', 'App\Http\Controllers\UserController');
 Route::put('/microposts/{id}/addLikes', [MicropostController::class, 'addLikes']);
 Route::apiResource('/microposts', 'App\Http\Controllers\MicropostController');
@@ -33,7 +33,7 @@ Route::apiResource('/microposts', 'App\Http\Controllers\MicropostController');
 
 //protected routes
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/users/{id}/microposts', [UserController::class, 'userShowMicroposts']);
+//    Route::get('/users/{id}/microposts', [UserController::class, 'userShowMicroposts']);
     //This works if user have a token. But this should work for a specific toke(user) not for all token.
 
 //    Route::apiResource('/users', 'App\Http\Controllers\UserController');
