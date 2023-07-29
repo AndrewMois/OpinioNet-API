04faa568bdd9d1e778deb2c41cacacb574bc4eae
peruso <michaelyuta@live.jp>
2023-07-29 17:09:43 -0400

    Created index() to return num of likes to post

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -6,15 +6,33 @@ use App\Models\Like;
 use Illuminate\Http\Request;
 use App\Models\Micropost;
 use App\Models\User;
+use PHPUnit\Framework\Constraint\Count;
 
 class LikeController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+    //Return Number of likes
     public function index()
     {
-        //
+        // $likes = Like::all();
+
+        $likes = Like::join('users', 'likes.user_id', '=', 'users.id')
+            // ->join('microposts', 'likes.micropost_id', '=', 'microposts.id')
+            ->select('likes.*', 'users.name as user_name')
+            ->orderByDesc('likes.created_at')
+            ->get();
+
+        $likesCount = $likes->count();
+
+        return response()->json([
+            'likes' => $likes,
+            'likes_count' => $likesCount,
+        ], 200);
+
+        //So far, only number of likes to be returned
+
     }
 
     /**
