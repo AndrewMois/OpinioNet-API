f7a96a829b88554b2f03282dc3ccfbe396565cf3
Andrew <amoiseyenko@ukr.net>
2023-08-01 11:41:10 -0400

    Function to return likes_count along with all microposts information

--- app/Http/Controllers/MicropostController.php
+++ app/Http/Controllers/MicropostController.php
@@ -22,7 +22,7 @@ class MicropostController extends Controller
         // Get all microposts with username. Use pagination to limit the number of records and allow Infinite Scroll
         $microposts = Micropost::join('users', 'microposts.user_id', '=', 'users.id')
             ->select('microposts.*', 'users.name as user_name')
-            ->withCount('likes') // Get the count of likes for each micropost
+            ->withCount('likes') // Get the count of likes for each micropost. This works because we have defined the likes relationship in the Micropost model
             ->with('likes') // Eager load the likes relationship
             ->orderByDesc('microposts.created_at') // To get the latest posts first
             ->paginate($perPage, ['*'], 'page', $page);
