13a00115c450745995f2fccad6e32040a0ddaf7b
peruso <michaelyuta@live.jp>
2023-08-02 10:41:39 -0400

    Created Model relationships with other tables

---
+++ app/Models/Vote.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Vote extends Model
+{
+    use HasFactory;
+
+    public function micropost(): BelongsTo
+    {
+        return $this->belongsTo(Micropost::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
