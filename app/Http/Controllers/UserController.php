171700d09784712c3ed220ba34d6544b52cbd675
peruso <michaelyuta@live.jp>
2023-08-05 18:54:13 -0400

    Adjusted Json response format

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,12 +26,25 @@ class UserController extends Controller
     {
         $user = User::find($id);
 
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
         $microposts = $user->microposts()
             ->withCount('likes')
             ->with('likes')
-            ->orderByDesc('created_at')
+            ->with('votes')
+            ->selectRaw("
+                microposts.*,
+                COUNT(CASE WHEN votes.status = 'Agree' THEN 1 END) as agree_count,
+                COUNT(CASE WHEN votes.status = 'Not Sure' THEN 1 END) as not_sure_count,
+                COUNT(CASE WHEN votes.status = 'Disagree' THEN 1 END) as disagree_count
+            ")
+            ->leftJoin('votes', 'microposts.id', '=', 'votes.micropost_id')
+            ->groupBy('microposts.id')
+            ->orderByDesc('microposts.created_at')
             ->get();
 
-        return response()->json($microposts, 200);
+        return response()->json(['data' => $microposts], 200);
     }
 }
